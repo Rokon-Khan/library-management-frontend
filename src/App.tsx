9041cf793f0de227cf9d91adb261f530af33b6bf
Rokon Khan <rokonjust@gmail.com>
2025-07-03 20:47:45 +0600

    Library Management Project Initial Set-Up Done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-center">Vite React Redux and TypeScript </h1>
+      <Button variant={"default"}>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="text-3xl font-bold text-center">
+        This is the Home Page
+      </div>
+    </div>
+  );
+}
