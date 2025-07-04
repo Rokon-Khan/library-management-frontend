1e5c2594ccc4e1494113236e14091f920a30122e
Rokon Khan <rokonjust@gmail.com>
2025-07-05 02:19:14 +0600

    All React RTK Query Set-Up for Api End Points

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import { Provider } from "react-redux";
 
 import { RouterProvider } from "react-router";
 import "./index.css";
-import store from "./redux/store.ts";
+
+import { store } from "./redux/store.ts";
 import router from "./routes/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { libraryApi } from "./fetures/libraryApi";
 
-const store = configureStore({
-  reducer: {},
+export const store = configureStore({
+  reducer: {
+    [libraryApi.reducerPath]: libraryApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(libraryApi.middleware),
 });
 
-export default store;
+setupListeners(store.dispatch);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
